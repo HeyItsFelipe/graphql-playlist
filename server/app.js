9ab62e2b59c7d8eb38306d3bc7e77ac7df7b72e8
Felipe Fernandez <heyitsfelipe@gmail.com>
2018-08-27 21:12:06 -0700

    Initial commit.

---
+++ server/app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const schema = require('./schema/schema');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+
+// allow cross-origin requests
+app.use(cors());
+
+// connect to mlab database
+// make sure to replace my db string & creds with your own
+mongoose.connect('mongodb://testUser:testUser1234@ds127342.mlab.com:27342/graphql-netninja');
+mongoose.connection.once('open', () => {
+    console.log('Connected to database.');
+});
+
+app.use('/graphql', graphqlHTTP({
+    schema,
+    graphiql: true 
+}));
+
+app.listen(4000, () => {
+    console.log('Now listening for requests on port 4000.')
+});
